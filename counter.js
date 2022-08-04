7b7e9b8401d97f3881e71f589eb7d19eb7aad936
shforeal <rivena14798@gmail.com>
2022-08-04 12:21:16 +0300

    changed

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@ export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
-    element.innerHTML = `count is ${counter}`
+    element.innerHTML = `count isee ${counter}`
   }
   element.addEventListener('click', () => setCounter(++counter))
   setCounter(0)
